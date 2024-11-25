b6462cc53f2791c4021c674dcd2ca718224cffc6
Jemal-G <jgeda@madisoncollege.edu>
2024-11-25 12:34:27 -0600

    Added extra content to App.js and index.css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Jemal!</h1>
+      <h1>Welcome to React App!</h1>
+      <p>This is a additional content.</p>
+      {/* First section */}
+      <div className="section-one">
+        <h2>My project Topics:</h2>
+        <ul>
+          <li>Creating and managing a GitHub repository</li>
+          <li>Using GitHub to trigger a CodePipeline deployment
+          </li>
+          <li>Setting up S3 to host a ReactJS application</li>
+          <li>React.js</li>
+          <li>AWS CI/CD</li>
+          <li>Web Development</li>
+        </ul>
+      </div>
+
+      {/* Second section */}
+      <div className="section-two">
+        <h2>About Me:</h2>
+        <p>I am passionate about building scalable web applications.</p>
+      </div>
     </div>
   );
 }
